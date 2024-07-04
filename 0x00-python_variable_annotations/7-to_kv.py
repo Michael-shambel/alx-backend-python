e598a688381cafb18eecca547c6ad969eadc2abb
michael-shambel <Michaelshambel10@gmail.com>
2024-07-04 21:20:32 +0300

    task 2

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@ type-annotated function to_kv that takes a string k and an int OR float v as arg
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float])-> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     funtion that take string and a union of integer and float and return the tuble of both
     """
