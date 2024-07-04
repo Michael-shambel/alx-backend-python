7933c2e6aae63cb2b8041ce5710e322acbf6908e
michael-shambel <Michaelshambel10@gmail.com>
2024-07-04 21:22:53 +0300

    task line overnumber

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -8,6 +8,7 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    funtion that take string and a union of integer and float and return the tuble of both
+    funtion that take string and a union of integer and float
+    and return the tuble of both
     """
     return (k, v**2)
