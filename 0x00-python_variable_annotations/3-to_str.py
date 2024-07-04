4fd58979149b0cf9efdddf6de444938217802fb9
michael-shambel <Michaelshambel10@gmail.com>
2024-07-04 17:32:10 +0300

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+type-annotated function to_str that takes a float n as argument and
+returns the string representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    this function accept the n float type and
+    return the string representation of itself
+    """
+    return "{}".format(n)
