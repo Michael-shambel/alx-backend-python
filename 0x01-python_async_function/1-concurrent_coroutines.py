d8e4e9224b497edf08f0842731b74b384f729957
michael-shambel <Michaelshambel10@gmail.com>
2024-07-08 20:20:35 +0300

    task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+async functionroutine that takes two argument and return
+the list of all the delays
+"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    take two arument and retunn the list of delay
+    """
+    list_delay = []
+    for _ in range(n):
+        delay = await wait_random(max_delay)
+        list_delay.append(delay)
+
+    return list_delay
