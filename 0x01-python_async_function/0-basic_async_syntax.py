5500b93f97ce06095a6acd6c2a8bdbb1f6bb6bb3
michael-shambel <Michaelshambel10@gmail.com>
2024-07-08 19:57:57 +0300

    task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+asynchronous coroutine that takes in aninteger argumet with default value
+and return it after some delay bitween 0 and max_delay
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    i = random.randint(0, max_delay)
+    await asyncio.sleep(i)
+    return i
