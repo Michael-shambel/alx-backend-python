07fcb7532d70bdf7381aa169605c4928e1919f69
michael-shambel <Michaelshambel10@gmail.com>
2024-07-09 17:16:45 +0300

    tas 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,3 +4,12 @@ function coroutine take no argumets. 10 times
 """
 import asyncio
 import random
+
+
+async def async_generator():
+    """
+    function that loop ten times and wait 1 second
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
