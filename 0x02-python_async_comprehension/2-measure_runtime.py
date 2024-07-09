059d65449542f9e202cf18d735afdb137fad90f7
michael-shambel <Michaelshambel10@gmail.com>
2024-07-09 23:32:33 +0300

    task 2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+write a measure_runtime coroutine that will execute async_comprehension
+ four times in parallel using asyncio.gather.
+"""
+import asyncio
+import time
+import random
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    write a measure_runtime coroutine that will execute async_comprehension
+    four times in parallel using asyncio.gather.
+    """
+    st = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    ed = time.perf_counter()
+    return st - ed
