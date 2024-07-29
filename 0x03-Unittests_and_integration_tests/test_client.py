b4bee6c5463ab41de3ec6d548b2bcd9994b2c38a
michael-shambel <Michaelshambel10@gmail.com>
2024-07-30 00:35:34 +0300

    task 11232aa

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+"""
+from logging import exception
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map, get_json, memoize
+from unittest.mock import patch
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    @parameterized.expand([
+        ('google', 'google'),
+        ('abc', 'abc')
+    ])
+    @patch('client.get_json')
+    def test_org(self, orgs, mock_get_json):
+        client = GithubOrgClient(orgs)
+        client.org()
+        mock_get_json.assert_called_once_with(f'https://api.github.com/orgs/{orgs}')
