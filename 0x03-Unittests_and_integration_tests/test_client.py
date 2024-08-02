20d70376607fa4507aa1dde5965e50a85bc8e84b
michael-shambel <Michaelshambel10@gmail.com>
2024-08-02 14:48:22 +0300

    task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """
+GithubOrgClient is a class that interacts with the GitHub API to fetch
+information about GitHub organizations.
 """
 from logging import exception
 import unittest
@@ -22,11 +24,19 @@ class TestGithubOrgClient(unittest.TestCase):
             f'https://api.github.com/orgs/{orgs}'
             )
 
-    def test_public_repos_url(self):
-        with patch.object(GithubOrgClient):
-            client = GithubOrgClient()
-            result = client._public_repos_url()
-            self.assertEqual(result, ['repo1', 'repo2'])
+    @patch.object(GithubOrgClient, 'org', new_callable=property)
+    def test_public_repos_url(self, mock_org):
+        mock_org.return_value = {
+            "repos_url": "https://api.github.com/orgs/example/repos"
+        }
+        client = GithubOrgClient('example')
+        expected_url = "https://api.github.com/orgs/example/repos"
+        result = client._public_repos_url()
+        self.assertEqual(result, expected_url)
+        # with patch.object(GithubOrgClient):
+        #     client = GithubOrgClient()
+        #     result = client._public_repos_url()
+        #     self.assertEqual(result, ['repo1', 'repo2'])
 
     @patch('client.get_json')
     def test_public_repos(self, mock_get_json):
